155886a523d0ca279c4a305f1f1ffd1094523a85
Tor Løkken <tor.lokken@gmail.com>
2016-12-28 09:48:46 +0100

    Added morse translator

---
+++ src/main/resources/lib/enonic/morse-translator/index.js
@@ -0,0 +1,68 @@
+exports.translate = function(text) {
+    var translatedText = '';
+    var characterSeparator = ' ';
+    var characterMap = getMorseCharacterMap();
+
+    // Iterate through each character in text
+    for (var i = 0, j = text.length; i < j; i++) {
+        var character = text.toLowerCase()[i];
+        var translatedCharacter = '?';
+
+        if (character in characterMap) {
+            translatedCharacter = characterMap[character];
+        }
+
+        translatedText += translatedCharacter;
+
+        // If not last character in text
+        if (i < j) {
+            translatedText += characterSeparator;
+        }
+    }
+
+    return translatedText;
+};
+
+function getMorseCharacterMap() {
+    var wordSeparator = '|';
+
+    return {
+        'a': '.-',
+        'b': '-...',
+        'c': '-.-.',
+        'd': '-..',
+        'e': '.',
+        'f': '..-.',
+        'g': '--.',
+        'h': '....',
+        'i': '..',
+        'j': '.---',
+        'k': '-.-',
+        'l': '.-..',
+        'm': '--',
+        'n': '-.',
+        'o': '---',
+        'p': '.--.',
+        'q': '--.-',
+        'r': '.-.',
+        's': '...',
+        't': '-',
+        'u': '..-',
+        'v': '...-',
+        'w': '.--',
+        'x': '-..-',
+        'y': '-.--',
+        'z': '--..',
+        '1': '.----',
+        '2': '..---',
+        '3': '...--',
+        '4': '....-',
+        '5': '.....',
+        '6': '-....',
+        '7': '--...',
+        '8': '---..',
+        '9': '----.',
+        '0': '-----',
+        ' ': wordSeparator
+    };
+}
